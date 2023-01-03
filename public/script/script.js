9bf10081627455c61bd51bc3733101d1865f80e3
3JloyCJloHuk <kostyaopanasuk@gmail.com>
2023-01-03 18:51:53 +0200

    3

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+    //humburger menu
     const buttonOpenMenu = document.querySelector('.header__button-open'),
         buttonCloseMenu = document.querySelector('.header__button-close'),
         headerMenu = document.querySelector('.header__menu');
@@ -9,4 +10,41 @@ window.addEventListener('DOMContentLoaded', function () {
     buttonCloseMenu.addEventListener('click', function () {
         headerMenu.classList.remove('header__menu--active');
     });
+
+    //slider
+
+    const onClickShowSlides = document.querySelectorAll('.how__item'),
+        slides = document.querySelectorAll('.how__slide'),
+        dotsParent = document.querySelector('.how__dots'),
+        dots = [];
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('how__dot');
+        dotsParent.append(dot);
+        dots.push(dot);
+    }
+
+    dots[0].classList.add('how__dot--active')
+    slides[0].classList.add('how__slide--active')
+
+    onClickShowSlides.forEach((elem, i) => elem.addEventListener('click', function () {
+        changeSlide();
+        slides[i].classList.add('how__slide--active');
+        dots[i].classList.add('how__dot--active');
+    }));
+    dots.forEach((elem, i) => elem.addEventListener('click', function () {
+        changeSlide();
+        slides[i].classList.add('how__slide--active');
+        dots[i].classList.add('how__dot--active');
+    }));
+
+    function changeSlide() {
+        slides.forEach(elem => {
+            elem.classList.remove('how__slide--active');
+        })
+        dots.forEach(elem => {
+            elem.classList.remove('how__dot--active');
+        })
+    }
 });
